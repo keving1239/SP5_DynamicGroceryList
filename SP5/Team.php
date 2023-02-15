fb85776cfd45be24ca770f0d16f34e684bba938a
Magnicheri <88209969+Magnicheri@users.noreply.github.com>
2023-02-15 06:41:58 -0800

    Add files via upload

---
+++ SP5/Team.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>DB Project</title>
+    <meta name="author" content="Phillip Magnicheri">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link href="SP5_css.css" rel="stylesheet" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <div class ="banner">
+        <img src = "https://i.pinimg.com/originals/f3/8c/22/f38c22bbe8f5e749c7a99bce6a3f7349.jpg" alt="Banner" width = 100% height = 200px>
+    <h1>  <a href = "Team.php">About/Contact</a></h1>
+  <div class = "navbar">
+  <a href="SP5_home.php">Home</a>
+  <a href="App-Demo.php">Demo</a>
+  <a href="Resources.php">Resources</a>
+  <a href = "Team.php">About/Contact</a>	
+  </div></div>
+
+  <div class="image-grid">
+
+        <img src = "https://lh3.googleusercontent.com/XGCx-C6I4z8a2UQVOrTKs5wMTty7nskA7uLhUvqaeQrRv-aa4ENZCGd0-o2QzSFVX2x2BcLkzoYbidqHMW2IucSYTBqLJZwUePRAgnSTwmzWsaUlNaTu-vkmaPx9rZsk9v8uQnUb=w2400" alt="Phillip" >
+			<ul>
+				<li>Phillip Magnicheri</li>
+				<li>Team Lead, Secondary Developer</li>
+				<li>770-882-8488</li>
+				<li>pmagnicheri@gmail.com</li>
+				<li>pmagnich@students.kennesaw.edu</li>
+			</ul>
+        <img src = "https://lh3.googleusercontent.com/3FHeIluUNQY6BUOQRvOWchoRn4nbiz61CchyvmWHCeX23UNK2ftUfkj6j_KYhb7w2YVZje4X93w2PrARVYg2Bc5WOU5fFZyqDkgq4otBCrcnP9HdEqrAHH3lSEEkR5jjWhJ6bJ7m=w2400" alt="Kevin" >
+			<ul>
+				<li>Kevin Galdamez</li>
+				<li>Lead Developer, Secondary Documentation</li>
+				<li>912-242-5668</li>
+				<li>kevingaldamez699@gmail.com</li>
+				<li>kgaldame@students.kennesaw.edu/li>
+			</ul>
+
+        <img src = "https://lh3.googleusercontent.com/Qo1YLhMmb8e03Z-JTUvdqb2Xr22rapvfyhC_VEg_WxoFaNIRw8G1xbjj3oQ2Mmo8m9h9gP-lg8BWsTANDkAY0VDOdDtkSBoitiDLcS9RyfAb_qK9_wx6iHe5kQeFW_94VfyYO4mv=w2400" alt="Scott"  >
+			<ul>
+				<li>Scott McCandless</li>
+				<li>Lead Research, Lead Testerli>
+				<li>478-262-5104li>
+				<li>scottmccandless@ymail.com</li>
+				<li>smccand2@students.kennesaw.edu/li>
+			</ul>
+
+        <img src = "https://lh3.googleusercontent.com/RjhaK457p0StF8XaMuHAl644Tci28YV-JlXPrAvhbejmyMe8vXI6fD1CZe735HKHj48F-NoEVOgJesIaSc5Rsmt6KohS3GvWyUxsXgA4s4hTVn_toMaH98yXEp_HfJDqYPCg-gY6=s252-p-k" alt="Ashia" >
+			<ul>
+				<li>Ashia Hawkins</li>
+				<li>Lead Documentation, Secondary Research</li>
+				<li>678-431-4170li>
+				<li>ashia.hawkins23@gmail.com</li>
+				<li>ahawki59@students.kennesaw.edu</li>
+			</ul>
+	 </div>
+
+
+<div class = "footer">
+  <img src = "https://i.pinimg.com/originals/f3/8c/22/f38c22bbe8f5e749c7a99bce6a3f7349.jpg" alt="Banner" width = 100% height = 50px>
+  </div> 
+
+
+   
+  </body>
+</html>
